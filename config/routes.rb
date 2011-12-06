fd27094e3ca9f949df01f87262900cc612b58e36
krudy470 <cameron.chang@orionvm.com.au>
2011-12-06 11:46:45 +1100

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ FirstApp::Application.routes.draw do
   resources :microposts
 
   resources :users
+  
+  get "home/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
