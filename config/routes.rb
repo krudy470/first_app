5445679c50738c4bcdddf3aefdcb8a75dea6163d
krudy470 <cameron.chang@orionvm.com.au>
2011-12-06 11:48:00 +1100

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ FirstApp::Application.routes.draw do
   resources :microposts
 
   resources :users
-  
-  get "home/index"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +53,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
